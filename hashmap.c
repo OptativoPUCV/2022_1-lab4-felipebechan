853aab99e416362bcac0c3b930a00ec9c590b25c
Replit user <>
2023-05-15 15:13:32 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -110,6 +110,19 @@ Pair *searchMap(HashMap *map, char *key) {
   return NULL;
 }
 
-Pair *firstMap(HashMap *map) { return NULL; }
+Pair *firstMap(HashMap *map) {
+  // itera
+  for (int i = 0; i < map->capacity; i++) {
+    // si current no esta vacio tiene key
+    if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+      // maps current a current
+      map->current = i;
+
+      return map->buckets[i];
+    }
+  }
+
+  return NULL;
+}
 
 Pair *nextMap(HashMap *map) { return NULL; }
